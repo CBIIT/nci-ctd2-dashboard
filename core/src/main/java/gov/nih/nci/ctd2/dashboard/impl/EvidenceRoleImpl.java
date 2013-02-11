6e47eb633b83ca9c9d0cbf8cf16c9a99b42bface
Benjamin Gross <benjamin.gross@gmail.com>
2013-02-11 16:45:12 -0500

    added evidence, subject class name property to EvidenceRole and SubjectRole respectively which indicated which kind of Evidence or Subject class should be create based on metadata

--- core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/EvidenceRoleImpl.java
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/EvidenceRoleImpl.java
@@ -3,6 +3,7 @@ package gov.nih.nci.ctd2.dashboard.impl;
 import gov.nih.nci.ctd2.dashboard.model.EvidenceRole;
 import org.hibernate.annotations.Proxy;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,4 +11,14 @@ import javax.persistence.Table;
 @Table(name = "evidence_role")
 @Proxy(proxyClass = EvidenceRole.class)
 public class EvidenceRoleImpl extends DashboardEntityImpl implements EvidenceRole {
+    private String evidenceClassName;
+
+    @Column(length = 32, nullable = false)
+    public String getEvidenceClassName() {
+        return evidenceClassName;
+    }
+
+    public void setEvidenceClassName(String evidenceClassName) {
+        this.evidenceClassName = evidenceClassName;
+    }
 }
