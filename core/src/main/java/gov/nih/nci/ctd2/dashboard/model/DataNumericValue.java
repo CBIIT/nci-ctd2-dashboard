d09415019f37abbc380869f3f35f1e09511a5e53
Benjamin Gross <benjamin.gross@gmail.com>
2013-02-12 17:21:17 -0500

    added missing methods from interface

--- core/src/main/java/gov/nih/nci/ctd2/dashboard/model/DataNumericValue.java
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/model/DataNumericValue.java
@@ -1,4 +1,8 @@
 package gov.nih.nci.ctd2.dashboard.model;
 
 public interface DataNumericValue extends Evidence {
+    Number getNumericValue();
+	void setNumericValue(Number numericValue);
+	ValueType getValueType();
+	void setValueType(ValueType valueType);
 }
