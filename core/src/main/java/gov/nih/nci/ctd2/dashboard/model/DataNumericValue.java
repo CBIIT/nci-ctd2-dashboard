34294a6c5260241ff45e59ec06a2ad4ce9330311
Benjamin Gross <benjamin.gross@gmail.com>
2013-02-14 14:06:53 -0500

    Removed value type class and pushed units property into DataNumericValue.  We may want value, url, and mime type classes in the future, but for now keep it simple

--- core/src/main/java/gov/nih/nci/ctd2/dashboard/model/DataNumericValue.java
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/model/DataNumericValue.java
@@ -3,6 +3,6 @@ package gov.nih.nci.ctd2.dashboard.model;
 public interface DataNumericValue extends Evidence {
     Number getNumericValue();
 	void setNumericValue(Number numericValue);
-	ValueType getValueType();
-	void setValueType(ValueType valueType);
+    String getUnit();
+    void setUnit(String unit);
 }
