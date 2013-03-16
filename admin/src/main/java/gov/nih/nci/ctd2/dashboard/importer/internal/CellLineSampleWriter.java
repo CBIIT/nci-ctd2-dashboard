bc7d3427f99e23b92946fccd19ae5414ab333206
n1zea144 <n1zea144@localhost>
2013-03-15 23:32:08 -0400

    Optimization/refactoring to cell sample import.  We do not persist CellSample instances until they are completely formed (synonyms, xrefs, etc).  We should also move to stateless session while importing all data.

--- admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/CellLineSampleWriter.java
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/CellLineSampleWriter.java
@@ -1,26 +1,12 @@
 package gov.nih.nci.ctd2.dashboard.importer.internal;
 
 import gov.nih.nci.ctd2.dashboard.model.CellSample;
-import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @Component("cellLineSampleWriter")
 public class CellLineSampleWriter implements ItemWriter<CellSample> {
-
-    @Autowired
-	private DashboardDao dashboardDao;
- 
-	private static final Log log = LogFactory.getLog(CellLineSampleWriter.class);
- 
-	public void write(List<? extends CellSample> items) throws Exception {
-		for (CellSample cellSample : items) {
-			//log.info("Storing cell sample: " + cellSample.getDisplayName());
-			dashboardDao.save(cellSample);
-		}
-	}
+	// optimization - all writing is done in CellLineDataWriter
+	public void write(List<? extends CellSample> items) throws Exception {}
 }
