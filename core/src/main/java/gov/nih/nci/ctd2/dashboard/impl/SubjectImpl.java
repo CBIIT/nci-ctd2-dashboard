308d1bffe37738ee47ab93f663ace0df820137a8
n1zea144 <n1zea144@localhost>
2013-03-29 12:38:14 -0400

    now using LinkedHashSet for Xref and Synonym collections to ensure proper ordering when persisting xrefs and synonyms.  This was modivated by desired to display order of cell sample synonyms in order of name type priority.  We may have to tweak DashboardDao class to properly populate collections on select from db.

--- core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/SubjectImpl.java
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/SubjectImpl.java
@@ -8,15 +8,15 @@ import org.hibernate.annotations.LazyCollectionOption;
 import org.hibernate.annotations.Proxy;
 
 import javax.persistence.*;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Entity
 @Proxy(proxyClass = Subject.class)
 @Table(name = "subject")
 public class SubjectImpl extends DashboardEntityImpl implements Subject {
-    private Set<Synonym> synonyms = new HashSet<Synonym>();
-    private Set<Xref> xrefs = new HashSet<Xref>();
+    private Set<Synonym> synonyms = new LinkedHashSet<Synonym>();
+    private Set<Xref> xrefs = new LinkedHashSet<Xref>();
 
     @LazyCollection(LazyCollectionOption.FALSE)
     @OneToMany(targetEntity = SynonymImpl.class, cascade = CascadeType.ALL)
