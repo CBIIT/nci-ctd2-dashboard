cd942b073081d117cf999356dc4d663449f97dcb
n1zea144 <n1zea144@localhost>
2013-04-01 09:51:50 -0400

    Now making synonym from lineage property

--- admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/CellLineNameFieldSetMapper.java
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/CellLineNameFieldSetMapper.java
@@ -59,7 +59,8 @@ public class CellLineNameFieldSetMapper implements FieldSetMapper<CellSample> {
 					// cell line name types are ordered by priority in the cell sample name file
 					// if this is our first synonym, it has the highest priority -
 					// in which case we should use it as the cell sample display name
-					if (cellSample.getSynonyms().size() == 0) cellSample.setDisplayName(cellSampleName);
+					// we've created a synonym from lineage in CellLineSampleFieldSetMapper, so chk size == 1
+					if (cellSample.getSynonyms().size() == 1) cellSample.setDisplayName(cellSampleName);
 					cellSample.getSynonyms().add(synonym);
 				}
 			}
