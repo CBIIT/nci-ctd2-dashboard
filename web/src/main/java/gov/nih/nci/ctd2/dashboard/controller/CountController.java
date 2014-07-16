f5d85db14930e13ff3bf96966a0f8fe90362a4eb
B. Arman Aksoy <arman@aksoy.org>
2014-07-16 00:02:33 -0400

    extracted the common entity-get method from the list/count controllers out to a new utility class -- also provided even better caching.

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/CountController.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/CountController.java
@@ -6,6 +6,7 @@ import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
 import gov.nih.nci.ctd2.dashboard.model.*;
 import gov.nih.nci.ctd2.dashboard.util.DateTransformer;
 import gov.nih.nci.ctd2.dashboard.util.ImplTransformer;
+import gov.nih.nci.ctd2.dashboard.util.WebServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,7 +27,7 @@ import java.util.List;
 @RequestMapping("/count")
 public class CountController {
     @Autowired
-    private DashboardDao dashboardDao;
+    private WebServiceUtil webServiceUtil;
 
     @Transactional
     @RequestMapping(value="{type}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
@@ -34,53 +35,7 @@ public class CountController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
 
-        List<? extends DashboardEntity> entities = new ArrayList<DashboardEntity>();
-        if(type.equalsIgnoreCase("submission")) {
-            if(filterBy != null) {
-                SubmissionCenter submissionCenter = dashboardDao.getEntityById(SubmissionCenter.class, filterBy);
-                if(submissionCenter != null) {
-                    entities = dashboardDao.findSubmissionBySubmissionCenter(submissionCenter);
-                }
-            } else {
-                entities = dashboardDao.findEntities(Submission.class);
-            }
-        } else if(type.equalsIgnoreCase("observation")) {
-            if(filterBy != null) {
-                Submission submission = dashboardDao.getEntityById(Submission.class, filterBy);
-                if(submission != null) {
-                    entities = dashboardDao.findObservationsBySubmission(submission);
-                } else {
-                    Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
-                    if(subject != null) {
-                        ArrayList<Observation> observations = new ArrayList<Observation>();
-                        for (ObservedSubject observedSubject : dashboardDao.findObservedSubjectBySubject(subject)) {
-                            observations.add(observedSubject.getObservation());
-                        }
-                        entities = observations;
-                    }
-                }
-            } else {
-                entities = dashboardDao.findEntities(Observation.class);
-            }
-        } else if(type.equals("center")) {
-            entities = dashboardDao.findEntities(SubmissionCenter.class);
-        } else if(type.equals("observedsubject") && filterBy != null) {
-            Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
-            if(subject != null) {
-                entities = dashboardDao.findObservedSubjectBySubject(subject);
-            } else {
-                Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
-                if(observation != null) {
-                    entities = dashboardDao.findObservedSubjectByObservation(observation);
-                }
-            }
-        } else if(type.equals("observedevidence") && filterBy != null) {
-            Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
-            if(observation != null) {
-                entities = dashboardDao.findObservedEvidenceByObservation(observation);
-            }
-        }
-
+        List<? extends DashboardEntity> entities = webServiceUtil.getDashboardEntities(type, filterBy);
         JSONSerializer jsonSerializer = new JSONSerializer()
                 .transform(new ImplTransformer(), Class.class)
                 .transform(new DateTransformer(), Date.class)
--- web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/ListController.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/ListController.java
@@ -6,6 +6,7 @@ import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
 import gov.nih.nci.ctd2.dashboard.model.*;
 import gov.nih.nci.ctd2.dashboard.util.DateTransformer;
 import gov.nih.nci.ctd2.dashboard.util.ImplTransformer;
+import gov.nih.nci.ctd2.dashboard.util.WebServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpHeaders;
@@ -27,7 +28,7 @@ import java.util.List;
 @RequestMapping("/list")
 public class ListController {
     @Autowired
-    private DashboardDao dashboardDao;
+    private WebServiceUtil webServiceUtil;
 
     @Transactional
     @RequestMapping(value="{type}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
@@ -35,8 +36,7 @@ public class ListController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
 
-        List<? extends DashboardEntity> entities = getDashboardEntities(type, filterBy);
-
+        List<? extends DashboardEntity> entities = webServiceUtil.getDashboardEntities(type, filterBy);
         // TODO: Remove this and add a pagination option
         if(entities.size() > 500)
             entities = entities.subList(0, 499);
@@ -53,59 +53,4 @@ public class ListController {
         );
     }
 
-    @Cacheable(value = "entityCache")
-    private List<? extends DashboardEntity> getDashboardEntities(String type, Integer filterBy) {
-        List<? extends DashboardEntity> entities = new ArrayList<DashboardEntity>();
-        if(type.equalsIgnoreCase("submission")) {
-            if(filterBy != null) {
-                SubmissionCenter submissionCenter = dashboardDao.getEntityById(SubmissionCenter.class, filterBy);
-                if(submissionCenter != null) {
-                    entities = dashboardDao.findSubmissionBySubmissionCenter(submissionCenter);
-                }
-            } else {
-                entities = dashboardDao.findEntities(Submission.class);
-            }
-        } else if(type.equalsIgnoreCase("observation")) {
-            if(filterBy != null) {
-                Submission submission = dashboardDao.getEntityById(Submission.class, filterBy);
-                if(submission != null) {
-                    entities = dashboardDao.findObservationsBySubmission(submission);
-                } else {
-                    Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
-                    if(subject != null) {
-                        ArrayList<Observation> observations = new ArrayList<Observation>();
-                        for (ObservedSubject observedSubject : dashboardDao.findObservedSubjectBySubject(subject)) {
-                            observations.add(observedSubject.getObservation());
-                        }
-                        entities = observations;
-                    }
-                }
-            } else {
-                entities = dashboardDao.findEntities(Observation.class);
-            }
-        } else if(type.equals("center")) {
-            entities = dashboardDao.findEntities(SubmissionCenter.class);
-        } else if(type.equals("observedsubject") && filterBy != null) {
-            Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
-            if(subject != null) {
-                entities = dashboardDao.findObservedSubjectBySubject(subject);
-            } else {
-                Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
-                if(observation != null) {
-                    entities = dashboardDao.findObservedSubjectByObservation(observation);
-                }
-            }
-        } else if(type.equals("observedevidence") && filterBy != null) {
-            Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
-            if(observation != null) {
-                entities = dashboardDao.findObservedEvidenceByObservation(observation);
-            }
-        } else if(type.equals("observationtemplate") && filterBy != null) {
-            SubmissionCenter submissionCenter = dashboardDao.getEntityById(SubmissionCenter.class, filterBy);
-            if(submissionCenter != null) {
-                entities = dashboardDao.findObservationTemplateBySubmissionCenter(submissionCenter);
-            }
-        }
-        return entities;
-    }
 }
