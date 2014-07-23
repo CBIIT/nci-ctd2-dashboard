6018149668a06446844412a2cb8ea8e77a9ec9d7
B. Arman Aksoy <arman@aksoy.org>
2014-07-23 01:05:51 -0400

    the subject observations are now capped when needed and also sorted by their tier (desc), date (asc) and summary (asc).

---
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/dao/DashboardDao.java
@@ -0,0 +1,53 @@
+package gov.nih.nci.ctd2.dashboard.dao;
+
+import gov.nih.nci.ctd2.dashboard.model.*;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface DashboardDao {
+    void save(DashboardEntity entity);
+    void update(DashboardEntity entity);
+    void delete(DashboardEntity entity);
+    <T extends DashboardEntity> T getEntityById(Class<T> entityClass, Integer id);
+    Long countEntities(Class<? extends DashboardEntity> entityClass);
+    DashboardFactory getDashboardFactory();
+    void setDashboardFactory(DashboardFactory dashboardFactory);
+    <T extends DashboardEntity> List<T> findEntities(Class<T> entityClass);
+    List<Gene> findGenesByEntrezId(String entrezId);
+    List<Gene> findGenesBySymbol(String symbol);
+    List<Protein> findProteinsByUniprotId(String uniprotId);
+    List<Transcript> findTranscriptsByRefseqId(String refseqId);
+    List<CellSample> findCellSampleByAnnoType(String type);
+    List<CellSample> findCellSampleByAnnoSource(String source);
+    List<CellSample> findCellSampleByAnnoName(String name);
+    List<CellSample> findCellSampleByAnnotation(Annotation annotation);
+    List<TissueSample> findTissueSampleByName(String name);
+    List<Compound> findCompoundsByName(String compoundName);
+    List<Compound> findCompoundsBySmilesNotation(String smilesNotation);
+    List<AnimalModel> findAnimalModelByName(String animalModelName);
+    List<Subject> findSubjectsByXref(String databaseName, String databaseId);
+    List<Subject> findSubjectsByXref(Xref xref);
+    List<Organism> findOrganismByTaxonomyId(String taxonomyId);
+    List<SubjectWithOrganism> findSubjectByOrganism(Organism organism);
+    List<Subject> findSubjectsBySynonym(String synonym, boolean exact);
+    ObservedSubjectRole findObservedSubjectRole(String templateName, String columnName);
+    ObservedEvidenceRole findObservedEvidenceRole(String templateName, String columnName);
+    ObservationTemplate findObservationTemplateByName(String templateName);
+    SubmissionCenter findSubmissionCenterByName(String submissionCenterName);
+    List<Submission> findSubmissionBySubmissionCenter(SubmissionCenter submissionCenter);
+    List<Observation> findObservationsBySubmission(Submission submission);
+    List<ObservedSubject> findObservedSubjectBySubject(Subject subject);
+    List<ObservedSubject> findObservedSubjectByObservation(Observation observation);
+    List<ObservedEvidence> findObservedEvidenceByObservation(Observation observation);
+    void batchSave(Collection<? extends DashboardEntity> entities, int batchSize);
+    void createIndex(int batchSize);
+    List<DashboardEntity> search(String keyword);
+    List<Submission> findSubmissionByIsStory(boolean isSubmissionStory, boolean sortByPriority);
+    List<Submission> findSubmissionByObservationTemplate(ObservationTemplate observationTemplate);
+    Submission findSubmissionByName(String submissionName);
+    List<Gene> browseTargets(String startsWith);
+    List<Compound> browseCompounds(String startsWith);
+    List<ObservationTemplate> findObservationTemplateBySubmissionCenter(SubmissionCenter submissionCenter);
+}
+
