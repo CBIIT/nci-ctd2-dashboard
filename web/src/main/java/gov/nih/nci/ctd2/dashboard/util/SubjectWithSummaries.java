6ed086b5a899610af3ce2f7441b948b789acee38
B. Arman Aksoy <arman@aksoy.org>
2014-10-28 23:30:58 -0400

    working on adjusting font sizes and font family.

---
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/util/SubjectWithSummaries.java
@@ -0,0 +1,62 @@
+package gov.nih.nci.ctd2.dashboard.util;
+
+import gov.nih.nci.ctd2.dashboard.model.Subject;
+
+import java.io.Serializable;
+
+public class SubjectWithSummaries implements Serializable {
+    private Subject subject;
+    private Integer numberOfSubmissions;
+    private Integer numberOfSubmissionCenters;
+    private Integer numberOfObservations;
+    private Integer maxTier;
+    private String role;
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Integer getNumberOfSubmissions() {
+        return numberOfSubmissions;
+    }
+
+    public void setNumberOfSubmissions(Integer numberOfSubmissions) {
+        this.numberOfSubmissions = numberOfSubmissions;
+    }
+
+    public Integer getNumberOfSubmissionCenters() {
+        return numberOfSubmissionCenters;
+    }
+
+    public void setNumberOfSubmissionCenters(Integer numberOfSubmissionCenters) {
+        this.numberOfSubmissionCenters = numberOfSubmissionCenters;
+    }
+
+    public Integer getNumberOfObservations() {
+        return numberOfObservations;
+    }
+
+    public void setNumberOfObservations(Integer numberOfObservations) {
+        this.numberOfObservations = numberOfObservations;
+    }
+
+    public Integer getMaxTier() {
+        return maxTier;
+    }
+
+    public void setMaxTier(Integer maxTier) {
+        this.maxTier = maxTier;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
