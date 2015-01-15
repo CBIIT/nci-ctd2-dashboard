ca16e3887139ad7acb31e5ff4fd9956c6fab0280
B. Arman Aksoy <arman@aksoy.org>
2015-01-15 12:57:17 -0500

    Added tag v2.10 for changeset 38774573d524

---
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/TRCshRNADataFieldSetMapper.java
@@ -0,0 +1,108 @@
+package gov.nih.nci.ctd2.dashboard.importer.internal;
+
+import gov.nih.nci.ctd2.dashboard.model.Xref;
+import gov.nih.nci.ctd2.dashboard.model.ShRna;
+import gov.nih.nci.ctd2.dashboard.model.Synonym;
+import gov.nih.nci.ctd2.dashboard.model.Organism;
+import gov.nih.nci.ctd2.dashboard.model.Transcript;
+import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
+import gov.nih.nci.ctd2.dashboard.model.DashboardFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindException;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+@Component("TRCshRNADataMapper")
+public class TRCshRNADataFieldSetMapper implements FieldSetMapper<ShRna> {
+
+	public static final String BROAD_SHRNA_DATABASE = "BROAD_SHRNA";
+	private static final String MISSING_ENTRY = "EMPTY";
+	private static final String REFSEQ_DELIMITER = ",";
+
+	private static final int CLONE_ID_COL_INDEX = 4;
+	private static final int TARGET_SEQ_COL_INDEX = 5;
+	private static final int CLONE_NAME_COL_INDEX = 8;
+	private static final int TAX_ID_COL_INDEX = 11;
+	private static final int TRANSCRIPT_ID_COL_INDEX = 15;
+
+    @Autowired
+    private DashboardFactory dashboardFactory;
+
+    @Autowired
+	private DashboardDao dashboardDao;
+
+    @Autowired
+	@Qualifier("TRCshRNAFilterMap")
+	private HashMap<String,String> tRCshRNAFilterMap;
+
+    private HashMap<String, Organism> organismMap = new HashMap<String, Organism>();
+    private HashMap<String, Transcript> transcriptMap = new HashMap<String, Transcript>();
+
+	public ShRna mapFieldSet(FieldSet fieldSet) throws BindException {
+
+        ShRna shRNA = dashboardFactory.create(ShRna.class);
+        shRNA.setType("shrna");
+
+		// sanity check
+		String cloneId = fieldSet.readString(CLONE_ID_COL_INDEX);
+		if (cloneId.equalsIgnoreCase(MISSING_ENTRY)) return shRNA;
+
+		// only process records that exist in our map
+		if (!tRCshRNAFilterMap.isEmpty() && !tRCshRNAFilterMap.containsKey(cloneId)) return shRNA;
+
+        shRNA.setDisplayName(fieldSet.readString(TARGET_SEQ_COL_INDEX));
+		// create synonym back to self
+		Synonym synonym = dashboardFactory.create(Synonym.class);
+		synonym.setDisplayName(fieldSet.readString(CLONE_NAME_COL_INDEX));
+		shRNA.getSynonyms().add(synonym);
+		// create xref back to broad
+		Xref xref = dashboardFactory.create(Xref.class);
+		xref.setDatabaseId(cloneId);
+		xref.setDatabaseName(BROAD_SHRNA_DATABASE);
+		shRNA.getXrefs().add(xref);
+		// set target seq
+		shRNA.setTargetSequence(fieldSet.readString(TARGET_SEQ_COL_INDEX));
+		// set organism
+        String taxonomyId = fieldSet.readString(TAX_ID_COL_INDEX);
+        Organism organism = organismMap.get(taxonomyId);
+        if (organism == null) {
+			List<Organism> organisms = dashboardDao.findOrganismByTaxonomyId(taxonomyId);
+			if (organisms.size() == 1) organism = organisms.get(0);
+            organismMap.put(taxonomyId, organism);
+        }
+		if (organism != null) shRNA.setOrganism(organism);
+		// set transcript
+		Transcript transcript = getTranscript(fieldSet.readString(TRANSCRIPT_ID_COL_INDEX));
+		if (transcript != null) shRNA.setTranscript(transcript);
+
+        return shRNA;
+	}
+
+	private Transcript getTranscript(String columnEntry) {
+
+		String[] transcriptIds = columnEntry.split(REFSEQ_DELIMITER);
+		// first look in hashmap
+		for (String transcriptId : transcriptIds) {
+			if (transcriptMap.containsKey(transcriptId)) {
+				return transcriptMap.get(transcriptId);
+			}
+		}
+		// now look in database
+		for (String transcriptId : transcriptIds) {
+			List<Transcript> transcripts = dashboardDao.findTranscriptsByRefseqId(transcriptId);
+			if (transcripts.size() == 1) {
+				transcriptMap.put(transcriptId, transcripts.get(0));
+				return transcripts.get(0);
+			}
+		}
+
+		// made it here
+		return null;
+	}
+}
---
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/util/WebServiceUtil.java
@@ -0,0 +1,95 @@
+package gov.nih.nci.ctd2.dashboard.util;
+
+import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
+import gov.nih.nci.ctd2.dashboard.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+public class WebServiceUtil {
+    @Autowired
+    private DashboardDao dashboardDao;
+
+    @Transactional
+    @Cacheable(value = "entityCache")
+    public List<? extends DashboardEntity> getDashboardEntities(String type, Integer filterBy) {
+        List<? extends DashboardEntity> entities = new ArrayList<DashboardEntity>();
+        if(type.equalsIgnoreCase("submission")) {
+            if(filterBy != null) {
+                SubmissionCenter submissionCenter = dashboardDao.getEntityById(SubmissionCenter.class, filterBy);
+                if(submissionCenter != null) {
+                    entities = dashboardDao.findSubmissionBySubmissionCenter(submissionCenter);
+                }
+            } else {
+                entities = dashboardDao.findEntities(Submission.class);
+            }
+        } else if(type.equalsIgnoreCase("observation")) {
+            if(filterBy != null) {
+                Submission submission = dashboardDao.getEntityById(Submission.class, filterBy);
+                if(submission != null) {
+                    entities = dashboardDao.findObservationsBySubmission(submission);
+                } else {
+                    Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
+                    if(subject != null) {
+                        ArrayList<Observation> observations = new ArrayList<Observation>();
+                        for (ObservedSubject observedSubject : dashboardDao.findObservedSubjectBySubject(subject)) {
+                            observations.add(observedSubject.getObservation());
+                        }
+                        Collections.sort(observations, new Comparator<Observation>() {
+                            @Override
+                            public int compare(Observation o1, Observation o2) {
+                                Integer tier2 = o2.getSubmission().getObservationTemplate().getTier();
+                                Integer tier1 = o1.getSubmission().getObservationTemplate().getTier();
+                                return tier2 - tier1;
+                            }
+                        });
+                        entities = observations;
+                    }
+                }
+            } else {
+                entities = dashboardDao.findEntities(Observation.class);
+            }
+        } else if(type.equals("center")) {
+            entities = dashboardDao.findEntities(SubmissionCenter.class);
+        } else if(type.equals("observedsubject") && filterBy != null) {
+            Subject subject = dashboardDao.getEntityById(Subject.class, filterBy);
+            if(subject != null) {
+                entities = dashboardDao.findObservedSubjectBySubject(subject);
+            } else {
+                Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
+                if(observation != null) {
+                    entities = dashboardDao.findObservedSubjectByObservation(observation);
+                }
+            }
+        } else if(type.equals("observedevidence") && filterBy != null) {
+            Observation observation = dashboardDao.getEntityById(Observation.class, filterBy);
+            if(observation != null) {
+                entities = dashboardDao.findObservedEvidenceByObservation(observation);
+            }
+        } else if(type.equals("observationtemplate") && filterBy != null) {
+            SubmissionCenter submissionCenter = dashboardDao.getEntityById(SubmissionCenter.class, filterBy);
+            if(submissionCenter != null) {
+                entities = dashboardDao.findObservationTemplateBySubmissionCenter(submissionCenter);
+            }
+        } else if(type.equals("role")) {
+            List<SubjectRole> sRoles = dashboardDao.findEntities(SubjectRole.class);
+            Collections.sort(sRoles, new Comparator<SubjectRole>() {
+                @Override
+                public int compare(SubjectRole o1, SubjectRole o2) {
+                    return o1.getDisplayName().compareTo(o2.getDisplayName());
+                }
+            });
+            entities = sRoles;
+        }
+
+        return entities;
+    }
+
+    @Transactional
+    @Cacheable(value = "exploreCache")
+    public List<SubjectWithSummaries> exploreSubjects(String keyword) {
+        return dashboardDao.findSubjectWithSummariesByRole(keyword, 1);
+    }
+}
