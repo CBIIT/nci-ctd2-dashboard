7805fc1c4251a95f94ba2d1c9c4ee16e40d2e260
B. Arman Aksoy <arman@aksoy.org>
2015-08-27 13:45:40 -0400

    add new method to specifically look up for cell lines

---
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/siRNADataFieldSetMapper.java
@@ -0,0 +1,93 @@
+package gov.nih.nci.ctd2.dashboard.importer.internal;
+
+import gov.nih.nci.ctd2.dashboard.model.ShRna;
+import gov.nih.nci.ctd2.dashboard.model.Synonym;
+import gov.nih.nci.ctd2.dashboard.model.Organism;
+import gov.nih.nci.ctd2.dashboard.model.Transcript;
+import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
+import gov.nih.nci.ctd2.dashboard.model.DashboardFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindException;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Component("siRNADataMapper")
+public class siRNADataFieldSetMapper implements FieldSetMapper<ShRna> {
+
+	private static final String REFSEQ_DELIMITER = " ";
+
+	private static final int REAGENT_NAME_COL_INDEX = 0;
+	private static final int TARGET_SEQ_COL_INDEX = 1;
+	private static final int TRANSCRIPT_ID_COL_INDEX = 2;
+	private static final int TARGET_GENE_COL_INDEX = 3;
+	private static final int ORGANISM_COL_INDEX = 4;
+
+    @Autowired
+    private DashboardFactory dashboardFactory;
+
+    @Autowired
+	private DashboardDao dashboardDao;
+
+    private HashMap<String, Organism> organismMap = new HashMap<String, Organism>();
+    private HashMap<String, Transcript> transcriptMap = new HashMap<String, Transcript>();
+
+	public ShRna mapFieldSet(FieldSet fieldSet) throws BindException {
+
+        ShRna shRNA = dashboardFactory.create(ShRna.class);
+        shRNA.setType("sirna");
+
+        shRNA.setDisplayName(fieldSet.readString(REAGENT_NAME_COL_INDEX));
+        shRNA.setReagentName(shRNA.getDisplayName());
+        // create synonym back to self
+		Synonym synonym = dashboardFactory.create(Synonym.class);
+		synonym.setDisplayName(fieldSet.readString(REAGENT_NAME_COL_INDEX));
+		shRNA.getSynonyms().add(synonym);
+		// set target seq
+		shRNA.setTargetSequence(fieldSet.readString(TARGET_SEQ_COL_INDEX));
+		// set organism
+	    if (fieldSet.readString(ORGANISM_COL_INDEX).equals("Homo sapiens")) {
+	        Organism organism = organismMap.get("9606");
+	        if (organism == null) {
+				List<Organism> organisms = dashboardDao.findOrganismByTaxonomyId("9606");
+				if (organisms.size() == 1) organism = organisms.get(0);
+	            organismMap.put("9606", organism);
+	        }
+			if (organism != null) shRNA.setOrganism(organism);
+		}
+		// set transcript
+		Transcript transcript = getTranscript(fieldSet.readString(TARGET_GENE_COL_INDEX),
+		                                      fieldSet.readString(TRANSCRIPT_ID_COL_INDEX));
+		if (transcript != null) shRNA.setTranscript(transcript);
+
+        return shRNA;
+	}
+
+	private Transcript getTranscript(String targetGene, String columnEntry) {
+
+		String[] transcriptIds = columnEntry.split(REFSEQ_DELIMITER);
+		// first look in hashmap
+		for (String transcriptId : transcriptIds) {
+			if (transcriptMap.containsKey(transcriptId)) {
+				return transcriptMap.get(transcriptId);
+			}
+		}
+		// now look in database
+		for (String transcriptId : transcriptIds) {
+			List<Transcript> transcripts = dashboardDao.findTranscriptsByRefseqId(transcriptId);
+			if (transcripts.size() == 1) {
+				Transcript t = transcripts.get(0);
+				if (t.getGene().getDisplayName().equals(targetGene)) {
+					transcriptMap.put(transcriptId, t);
+					return t;
+				}
+			}
+		}
+
+		// made it here
+		return null;
+	}
+}
---
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/ListController.java
@@ -0,0 +1,100 @@
+package gov.nih.nci.ctd2.dashboard.controller;
+
+import flexjson.JSONSerializer;
+import flexjson.transformer.AbstractTransformer;
+import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
+import gov.nih.nci.ctd2.dashboard.model.*;
+import gov.nih.nci.ctd2.dashboard.util.DateTransformer;
+import gov.nih.nci.ctd2.dashboard.util.ImplTransformer;
+import gov.nih.nci.ctd2.dashboard.util.WebServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+@Controller
+@RequestMapping("/list")
+public class ListController {
+    @Autowired
+    private WebServiceUtil webServiceUtil;
+
+    @Autowired
+    @Qualifier("maxNumberOfEntities")
+    private Integer maxNumberOfEntities = 100;
+
+    public WebServiceUtil getWebServiceUtil() {
+        return webServiceUtil;
+    }
+
+    public void setWebServiceUtil(WebServiceUtil webServiceUtil) {
+        this.webServiceUtil = webServiceUtil;
+    }
+
+    public Integer getMaxNumberOfEntities() {
+        return maxNumberOfEntities;
+    }
+
+    public void setMaxNumberOfEntities(Integer maxNumberOfEntities) {
+        this.maxNumberOfEntities = maxNumberOfEntities;
+    }
+
+    @Transactional
+    @RequestMapping(value="{type}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
+    public ResponseEntity<String> getSearchResultsInJson(
+            @PathVariable String type,
+            @RequestParam("filterBy") Integer filterBy,
+            @RequestParam(value = "getAll", required = false, defaultValue = "false") Boolean getAll
+    ) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+
+        List<? extends DashboardEntity> entities = webServiceUtil.getDashboardEntities(type, filterBy);
+        if(!getAll && entities.size() > getMaxNumberOfEntities()) {
+            entities = entities.subList(0, getMaxNumberOfEntities()-1);
+        }
+
+        JSONSerializer jsonSerializer = new JSONSerializer()
+                .transform(new ImplTransformer(), Class.class)
+                .transform(new DateTransformer(), Date.class)
+        ;
+
+        return new ResponseEntity<String>(
+                jsonSerializer.serialize(entities),
+                headers,
+                HttpStatus.OK
+        );
+    }
+
+    @Transactional
+    @RequestMapping(value="similar/{submissionId}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
+    public ResponseEntity<String> getSimilarSubmissionsInJson(@PathVariable Integer submissionId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+
+        JSONSerializer jsonSerializer = new JSONSerializer()
+                .transform(new ImplTransformer(), Class.class)
+                .transform(new DateTransformer(), Date.class)
+                ;
+        List<Submission> submissions = webServiceUtil.getSimilarSubmissions(submissionId);
+
+        return new ResponseEntity<String>(
+                jsonSerializer.serialize(submissions),
+                headers,
+                HttpStatus.OK
+        );
+    }
+
+}
