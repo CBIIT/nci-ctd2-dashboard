1887b1c0cfdb69b06c896f6a6048ab01f684e760
Zhou Ji <zj2122@columbia.edu>
2018-02-22 12:43:29 -0500

    implement get method for genes

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/JSONController.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/JSONController.java
@@ -5,6 +5,8 @@ import gov.nih.nci.ctd2.dashboard.dao.DashboardDao;
 import gov.nih.nci.ctd2.dashboard.model.*;
 import gov.nih.nci.ctd2.dashboard.util.DateTransformer;
 import gov.nih.nci.ctd2.dashboard.util.ImplTransformer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/get")
 public class JSONController {
+    private static final Log log = LogFactory.getLog(JSONController.class);
     @Autowired
     private DashboardDao dashboardDao;
 
@@ -33,30 +37,50 @@ public class JSONController {
 
     /* gene needs a separate method because it asks for different number of parameters */
     @Transactional
-    @RequestMapping(value="{type}/{species}/{symbol}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
-    public ResponseEntity<String> getGeneInJson(@PathVariable String type, @PathVariable String species, @PathVariable String symbol) {
-        System.out.println("DEBUG request "+type+" "+species+" "+symbol);
-
+    @RequestMapping(value = "{type}/{species}/{symbol}", method = { RequestMethod.GET,
+            RequestMethod.POST }, headers = "Accept=application/json")
+    public ResponseEntity<String> getGeneInJson(@PathVariable String type, @PathVariable String species,
+            @PathVariable String symbol) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
-        return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND); // FIXME for test only
+        if (!type.equalsIgnoreCase("gene")) {
+            log.info("query with wrong type");
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        List<Gene> genes = dashboardDao.findGenesBySymbol(symbol);
+        Gene gene = null;
+        for (Gene g : genes) {
+            if (g.getOrganism().getDisplayName().toLowerCase().startsWith(species)) {
+                gene = g;
+                break;
+            }
+        }
+
+        if (gene == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        } else {
+            JSONSerializer jsonSerializer = new JSONSerializer().transform(new ImplTransformer(), Class.class)
+                    .transform(new DateTransformer(), Date.class);
+            return new ResponseEntity<String>(jsonSerializer.deepSerialize(gene), headers, HttpStatus.OK);
+        }
     }
 
     @Transactional
-    @RequestMapping(value="{type}/{id}", method = {RequestMethod.GET, RequestMethod.POST}, headers = "Accept=application/json")
+    @RequestMapping(value = "{type}/{id}", method = { RequestMethod.GET,
+            RequestMethod.POST }, headers = "Accept=application/json")
     public ResponseEntity<String> getEntityInJson(@PathVariable String type, @PathVariable String id) {
         DashboardEntity entityById = null;
 
         Class<? extends DashboardEntity> clazz = Subject.class;
-        if(type.equalsIgnoreCase("subject")) {
+        if (type.equalsIgnoreCase("subject")) {
             clazz = Subject.class;
-        } else if(type.equalsIgnoreCase("submission")) {
+        } else if (type.equalsIgnoreCase("submission")) {
             clazz = Submission.class;
-        } else if(type.equalsIgnoreCase("observation")) {
+        } else if (type.equalsIgnoreCase("observation")) {
             clazz = Observation.class;
-        } else if(type.equals("observedsubject")) {
+        } else if (type.equals("observedsubject")) {
             clazz = ObservedSubject.class;
-        } else if(type.equals("observedevidence")) {
+        } else if (type.equals("observedevidence")) {
             clazz = ObservedEvidence.class;
         } else {
             clazz = type2class.get(type);
@@ -65,23 +89,17 @@ public class JSONController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
 
-        if(type2class.keySet().contains(type)) { // as the early step to implement stable links.
+        if (type2class.keySet().contains(type)) { // as the early step to implement stable links.
             entityById = dashboardDao.getEntity(clazz, id);
         } else {
             entityById = dashboardDao.getEntityById(clazz, Integer.parseInt(id));
         }
-        if(entityById == null) {
+        if (entityById == null) {
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
 
-        JSONSerializer jsonSerializer = new JSONSerializer()
-                .transform(new ImplTransformer(), Class.class)
-                .transform(new DateTransformer(), Date.class)
-                ;
-        return new ResponseEntity<String>(
-                jsonSerializer.deepSerialize(entityById),
-                headers,
-                HttpStatus.OK
-        );
+        JSONSerializer jsonSerializer = new JSONSerializer().transform(new ImplTransformer(), Class.class)
+                .transform(new DateTransformer(), Date.class);
+        return new ResponseEntity<String>(jsonSerializer.deepSerialize(entityById), headers, HttpStatus.OK);
     }
 }
