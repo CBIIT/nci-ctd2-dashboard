8234a8313deb4709954a35fda87e564c95be63e6
Zhou Ji <zj2122@columbia.edu>
2018-03-12 23:50:24 -0400

    add an no-op method to help JSONSerializer

--- core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/SubjectWithOrganismImpl.java
+++ core/src/main/java/gov/nih/nci/ctd2/dashboard/impl/SubjectWithOrganismImpl.java
@@ -10,10 +10,11 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="subject_with_organism")
+@Table(name = "subject_with_organism")
 @Proxy(proxyClass = SubjectWithOrganism.class)
 @Indexed
 public class SubjectWithOrganismImpl extends SubjectImpl implements SubjectWithOrganism {
+    private static final long serialVersionUID = -6851675098845211440L;
     private Organism organism;
 
     @ManyToOne(targetEntity = OrganismImpl.class)
@@ -23,5 +24,10 @@ public class SubjectWithOrganismImpl extends SubjectImpl implements SubjectWithO
 
     public void setOrganism(Organism organism) {
         this.organism = organism;
+    }
+
+    @Override
+    public void setStableURL(String dummy) {
+        // this no-op method needs to exist for flexjson.JSONSerializer to do the right thing for CellSample.
     }
 }
