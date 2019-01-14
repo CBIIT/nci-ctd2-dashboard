e0a05ad1a99fd7ad7b1e8387faf00f0d18fde9b6
Yiwen He <yiwenhe@gmail.com>
2019-01-14 17:00:19 -0500

    Updates for v3.0_20190104 release

--- tools/do_ctd2_dashboard_load.sh
+++ tools/do_ctd2_dashboard_load.sh
@@ -4,7 +4,8 @@ LOAD_OPTS=('t' 'am' 'cl' 'ts' 'cp' 'g' 'p' 'sh' 'si' 'cv' 'o' 'i' 'r')
 for opt in "${LOAD_OPTS[@]}"
 do
 #    CMD="time java $JAVA_OPTS -jar $CTD2_HOME/admin/target/dashboard-admin.jar -$opt"
-    CMD="java $JAVA_OPTS -jar $CTD2_HOME/admin/target/dashboard-admin.jar -$opt"
+#    CMD="java $JAVA_OPTS -jar $CTD2_HOME/admin/target/dashboard-admin.jar -$opt"
+    CMD="java -jar -Xmx2048m $CTD2_HOME/admin/target/dashboard-admin.jar -$opt"
     echo $CMD
     $CMD
     if [[ $? -ne 0 ]] ; then
