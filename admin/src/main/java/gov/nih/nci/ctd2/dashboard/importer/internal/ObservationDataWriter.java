6b5dcec2bc261eeb8833c2db0ce1a0862b1e23c9
Zhou Ji <zj2122@columbia.edu>
2019-05-01 10:56:53 -0400

    load observation data in parallel

--- admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/ObservationDataWriter.java
+++ admin/src/main/java/gov/nih/nci/ctd2/dashboard/importer/internal/ObservationDataWriter.java
@@ -18,6 +18,8 @@ import java.util.HashMap;
 @Component("observationDataWriter")
 public class ObservationDataWriter implements ItemWriter<ObservationData> {
 
+    private volatile int counter = 0;
+
     @Autowired
     private DashboardDao dashboardDao;
 
@@ -32,6 +34,7 @@ public class ObservationDataWriter implements ItemWriter<ObservationData> {
     private Map<String, Integer> observationIndex = new HashMap<String, Integer>();
 
     public void write(List<? extends ObservationData> items) throws Exception {
+        log.debug("submission counter=" + ++counter + "; number of observations=" + items.size());
         ArrayList<DashboardEntity> entities = new ArrayList<DashboardEntity>();
 
         StableURL stableURL = new StableURL();
@@ -47,6 +50,8 @@ public class ObservationDataWriter implements ItemWriter<ObservationData> {
                     observationIndex.put(submissionName, 0);
                 }
                 submissionCache.put(submissionCacheKey, submission);
+            } else {
+                observation.setSubmission(submissionCache.get(submissionCacheKey));
             }
             int index = observationIndex.get(submissionName);
             observationIndex.put(submissionName, observationIndex.get(submissionName) + 1);
@@ -56,7 +61,10 @@ public class ObservationDataWriter implements ItemWriter<ObservationData> {
             entities.addAll(observationData.observedEntities);
         }
 
-        dashboardDao.batchSave(entities, batchSize);
-        log.debug("ObservationData written");
+        /* Including different kinds of things in one collection is most likely not to be a good idea.
+        entities include: (1) observations, (2) evidences, (3) observed subjects and evidences,
+        and (4) submission (only one expected, and should be saved before all other things */
+        dashboardDao.batchSave(entities, 1000); // smaller batch size helps to prevent org.hibernate.PessimisticLockException
+        log.debug("ObservationData written " + entities.size());
     }
 }
