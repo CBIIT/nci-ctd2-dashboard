11e90fefa88b306f918a97df0ae8add9d62a47cf
Zhou Ji <zj2122@columbia.edu>
2020-05-18 20:12:45 -0400

    remove extraneous class properties. #429

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/api/CTD2Serializer.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/api/CTD2Serializer.java
@@ -8,16 +8,15 @@ import gov.nih.nci.ctd2.dashboard.util.ImplTransformer;
 public class CTD2Serializer {
     static public JSONSerializer createJSONSerializer() {
         JSONSerializer jsonSerializer = new JSONSerializer().exclude("observation_count.class").exclude("xref.class")
+                .exclude("observations.subject_list.class").exclude("observations.evidence_list.class")
                 .exclude("observations.subject_list.xref.class").transform(new ImplTransformer(), Class.class)
                 .transform(new SimpleDateTransformer(), Date.class)
                 .transform(new FieldNameTransformer("class"), "clazz")
                 .transform(new FieldNameTransformer("class"), "observations.subject_list.clazz")
                 .transform(new FieldNameTransformer("class"), "observations.evidence_list.clazz")
                 .transform(new ExcludeTransformer(), void.class).exclude("class").exclude("observations.class")
-                .exclude("observations.evidence_list.evidenceName")
-                .exclude("observations.evidence_list.columnName")
-                .exclude("observations.evidence_list.observationId")
-                .exclude("observations.subject_list.columnName");
+                .exclude("observations.evidence_list.evidenceName").exclude("observations.evidence_list.columnName")
+                .exclude("observations.evidence_list.observationId").exclude("observations.subject_list.columnName");
         return jsonSerializer;
     }
 }
