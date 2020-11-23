1b4e07b0a109b14c88e9062b7d480b6abfc9affc
Zhou Ji <zj2122@columbia.edu>
2020-11-22 23:09:51 -0500

    minor clean-up

--- web/src/main/webapp/js/ctd2.js
+++ web/src/main/webapp/js/ctd2.js
@@ -3562,7 +3562,7 @@
                             content: function () {
                                 return ctd2_role_definition[$(this).text()];
                             },
-                        })
+                        });
                     });
                     $("#explore-table").parent().width("100%");
                     $("#explore-table").width("100%");
