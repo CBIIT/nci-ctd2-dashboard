8da0e4be67c63c09de18fb42621f2860eb412d26
Zhou Ji <zj2122@columbia.edu>
2022-10-05 15:47:45 -0400

    make the tree collapsible on 'circles'

--- web/src/main/webapp/js/treeview.js
+++ web/src/main/webapp/js/treeview.js
@@ -1,107 +1,153 @@
-// based on https://observablehq.com/@d3/tree
-export default function Tree(data, { // data is either tabular (array of objects) or hierarchy (nested objects)
-    path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
-    id = Array.isArray(data) ? d => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
-    parentId = Array.isArray(data) ? d => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
-    children, // if hierarchical data, given a d in data, returns its children
-    tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
+// based on https://observablehq.com/@d3/collapsible-tree and https://observablehq.com/@d3/tree
+export default function(data, { // data is hierarchy (nested objects)
     sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
     label, // given a node d, returns the display name
     title, // given a node d, returns its hover text
     link, // given a node d, its link (if any)
     linkTarget = "_blank", // the target attribute for links (if any)
     width = 640, // outer width, in pixels
-    height, // outer height, in pixels
     r = 3, // radius of nodes
     padding = 1, // horizontal padding for first and last column
     fill = "#999", // fill for nodes
-    fillOpacity, // fill opacity for nodes
     stroke = "#555", // stroke for links
     strokeWidth = 1.5, // stroke width for links
     strokeOpacity = 0.4, // stroke opacity for links
-    strokeLinejoin, // stroke line join for links
-    strokeLinecap, // stroke line cap for links
     halo = "#fff", // color of label halo 
     haloWidth = 3, // padding around the labels
 } = {}) {
+    const xlink = link
+    const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x)
+    const margin = ({ top: 10, right: 120, bottom: 10, left: 40 })
 
-    // If id and parentId options are specified, or the path option, use d3.stratify
-    // to convert tabular data to a hierarchy; otherwise we assume that the data is
-    // specified as an object {children} with nested objects (a.k.a. the “flare.json”
-    // format), and use d3.hierarchy.
-    const root = path != null ? d3.stratify().path(path)(data) :
-        id != null || parentId != null ? d3.stratify().id(id).parentId(parentId)(data) :
-            d3.hierarchy(data, children);
+    const root = d3.hierarchy(data)
 
     // Sort the nodes.
     if (sort != null) root.sort(sort);
 
-    // Compute labels and titles.
-    const descendants = root.descendants();
-    const L = label == null ? null : descendants.map(d => label(d.data, d));
-
     // Compute the layout.
     const dx = 10;
     const dy = width / (root.height + padding);
-    tree().nodeSize([dx, dy])(root);
+    const tree = d3.tree().nodeSize([dx, dy])
 
-    // Center the tree.
-    let x0 = Infinity;
-    let x1 = -x0;
-    root.each(d => {
-        if (d.x > x1) x1 = d.x;
-        if (d.x < x0) x0 = d.x;
+    root.x0 = dy / 2;
+    root.y0 = 0;
+    root.descendants().forEach((d, i) => {
+        d.id = i;
+        d._children = d.children;
     });
 
-    // Compute the default height.
-    if (height === undefined) height = x1 - x0 + dx * 2;
-
     const svg = d3.create("svg")
-        .attr("viewBox", [-dy * padding / 2, x0 - dx, width, height])
-        .attr("width", width)
-        .attr("height", height)
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10);
-
-    svg.append("g")
+        .attr("viewBox", [-margin.left, -margin.top, width, dx])
+        .style("font", "10px sans-serif")
+        .style("user-select", "none")
+    const gLink = svg.append("g")
         .attr("fill", "none")
         .attr("stroke", stroke)
         .attr("stroke-opacity", strokeOpacity)
-        .attr("stroke-linecap", strokeLinecap)
-        .attr("stroke-linejoin", strokeLinejoin)
         .attr("stroke-width", strokeWidth)
-        .selectAll("path")
-        .data(root.links())
-        .join("path")
-        .attr("d", d3.linkHorizontal()
-            .x(d => d.y)
-            .y(d => d.x));
+    const gNode = svg.append("g")
+        .attr("cursor", "pointer")
+        .attr("pointer-events", "all")
 
-    const node = svg.append("g")
-        .selectAll("g")
-        .data(root.descendants())
-        .join("g")
-        .attr("transform", d => `translate(${d.y},${d.x})`);
+    function update(source) {
+        const duration = d3.event && d3.event.altKey ? 2500 : 250;
+        const nodes = root.descendants().reverse();
+        const links = root.links();
 
-    node.append("circle")
-        .attr("fill", d => d.children ? stroke : fill)
-        .attr("r", r);
+        // Compute the new tree layout.
+        tree(root);
 
-    if (title != null) node.append("title")
-        .text(d => title(d.data, d));
+        let left = root;
+        let right = root;
+        root.eachBefore(node => {
+            if (node.x < left.x) left = node;
+            if (node.x > right.x) right = node;
+        });
 
-    if (L) node.append("a")
-        .attr("xlink:href", link == null ? null : d => link(d.data, d))
-        .attr("target", link == null ? null : linkTarget)
-        .append("text")
-        .attr("dy", "0.32em")
-        .attr("x", d => d.children ? -6 : 6)
-        .attr("text-anchor", d => d.children ? "end" : "start")
-        .attr("paint-order", "stroke")
-        .attr("stroke", halo)
-        .attr("stroke-width", haloWidth)
-        .text((d, i) => L[i]);
+        const height = right.x - left.x + margin.top + margin.bottom;
+
+        const transition = svg.transition()
+            .duration(duration)
+            .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
+            .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
+
+        // Update the nodes ...
+        const node = gNode.selectAll("g")
+            .data(nodes, d => d.id);
+
+        // Enter any new nodes at the parent's previous position.
+        const nodeEnter = node.enter().append("g")
+            .attr("transform", d => `translate(${source.y0},${source.x0})`)
+            .attr("fill-opacity", 0)
+            .attr("stroke-opacity", 0)
+
+        nodeEnter.append("circle")
+            .attr("r", r)
+            .attr("fill", d => d._children ? stroke : fill)
+            .attr("stroke-width", 10)
+            .on("click", (event, d) => {
+                d.children = d.children ? null : d._children;
+                update(d);
+            })
+
+        nodeEnter.append("a")
+            .attr("xlink:href", xlink == null ? null : d => xlink(d.data, d))
+            .attr("target", xlink == null ? null : linkTarget)
+            .append("text")
+            .attr("dy", "0.31em")
+            .attr("x", d => d._children ? -6 : 6)
+            .attr("text-anchor", d => d._children ? "end" : "start")
+            .text(d => label(d.data, d))
+            .clone(true).lower()
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-width", haloWidth)
+            .attr("stroke", halo);
+
+        if (title != null) nodeEnter.append("title")
+            .text(d => title(d.data, d));
+
+        // Transition nodes to their new position.
+        const nodeUpdate = node.merge(nodeEnter).transition(transition)
+            .attr("transform", d => `translate(${d.y},${d.x})`)
+            .attr("fill-opacity", 1)
+            .attr("stroke-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        const nodeExit = node.exit().transition(transition).remove()
+            .attr("transform", d => `translate(${source.y},${source.x})`)
+            .attr("fill-opacity", 0)
+            .attr("stroke-opacity", 0);
+
+        // Update the links ...
+        const link = gLink.selectAll("path")
+            .data(links, d => d.target.id);
+
+        // Enter any new links at the parent's previous position.
+        const linkEnter = link.enter().append("path")
+            .attr("d", d => {
+                const o = { x: source.x0, y: source.y0 };
+                return diagonal({ source: o, target: o });
+            });
+
+        // Transition links to their new position.
+        link.merge(linkEnter).transition(transition)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition(transition).remove()
+            .attr("d", d => {
+                const o = { x: source.x, y: source.y };
+                return diagonal({ source: o, target: o });
+            });
+
+        // Stash the old positions for transition.
+        root.eachBefore(d => {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
+
+    update(root);
 
     return svg.node();
 }
