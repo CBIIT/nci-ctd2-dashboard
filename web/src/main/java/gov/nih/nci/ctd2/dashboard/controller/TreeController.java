ec503bae013b783df4f1204687e72be59ca32c07
Zhou Ji <zj2122@columbia.edu>
2022-10-12 15:55:18 -0400

    refactoring

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/TreeController.java
+++ web/src/main/java/gov/nih/nci/ctd2/dashboard/controller/TreeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,37 +27,25 @@ public class TreeController {
     private DashboardDao dashboardDao;
 
     @Transactional
-    @RequestMapping(value = "disease-context", method = { RequestMethod.GET }, headers = "Accept=application/json")
-    public ResponseEntity<String> getDiseaseContextTree() {
-        log.debug("request received by getDiseaseContextTree ");
+    @RequestMapping(value = "{tree_id}", method = { RequestMethod.GET }, headers = "Accept=application/json")
+    public ResponseEntity<String> getTree(@PathVariable String tree_id) {
+        log.debug("request received by getTree");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
 
-        Map<Integer, Integer> count = dashboardDao.tissueSampleCodeToObservationNumber();
-        Node tree = Hierarchy.DISEASE_CONTEXT.getTree(count);
-        dashboardDao.setTissueSampleLabels(tree);
-        JSONSerializer jsonSerializer = new JSONSerializer().exclude("*.class");
-        String json = "{}";
-        try {
-            json = jsonSerializer.deepSerialize(tree);
-        } catch (Exception e) {
-            e.printStackTrace();
+        Node tree = null;
+        if (tree_id.equals("disease-context")) {
+            Map<Integer, Integer> count = dashboardDao.tissueSampleCodeToObservationNumber();
+            tree = Hierarchy.DISEASE_CONTEXT.getTree(count);
+            dashboardDao.setTissueSampleLabels(tree);
+        } else if (tree_id.equals("evidence-type")) {
+            Map<Integer, Integer> count = dashboardDao.evidenceTypeToObservationNumber();
+            tree = Hierarchy.EXPERIMENTAL_EVIDENCE.getTree(count);
+            dashboardDao.setEvidenceLabels(tree);
+        } else {
+            log.warn("not supported tree_id: " + tree_id);
             return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
         }
-
-        return new ResponseEntity<String>(json, headers, HttpStatus.OK);
-    }
-
-    @Transactional
-    @RequestMapping(value = "evidence-type", method = { RequestMethod.GET }, headers = "Accept=application/json")
-    public ResponseEntity<String> getEvidenceTypeTree() {
-        log.debug("request received by getEvidenceTypeTree ");
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json; charset=utf-8");
-
-        Map<Integer, Integer> count = dashboardDao.evidenceTypeToObservationNumber();
-        Node tree = Hierarchy.EXPERIMENTAL_EVIDENCE.getTree(count);
-        dashboardDao.setEvidenceLabels(tree);
         JSONSerializer jsonSerializer = new JSONSerializer().exclude("*.class");
         String json = "{}";
         try {
